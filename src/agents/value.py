a8a63f022ed4f314ed8160b9ccaa1e41e315e0ab
AndreyMymrin <113207976+AndreyMymrin@users.noreply.github.com>
2026-03-03 19:37:48 +0300

    Value network made to be one linear layer

--- src/agents/value.py
+++ src/agents/value.py
@@ -5,11 +5,13 @@ import torch
 class ValueNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim=64):
         super().__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.out = nn.Linear(hidden_dim, 1)
+        #self.fc1 = nn.Linear(input_dim, hidden_dim)
+        #self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        #self.out = nn.Linear(hidden_dim, 1)
+        self.out = nn.Linear(input_dim, 1)
+
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
+        #x = torch.relu(self.fc1(x))
+        #x = torch.relu(self.fc2(x))
         return self.out(x)
